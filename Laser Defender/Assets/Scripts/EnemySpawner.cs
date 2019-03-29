d908b89474d37490e36b658eb09ea15a218770be
Jose V <44216801+WizardStrago@users.noreply.github.com>
2019-03-28 23:03:25 -0300

    Laser Defender game

---
+++ Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    [SerializeField] List<WaveConfig> waveConfigs;
+    [SerializeField] int startingWave = 0;
+    [SerializeField] bool looping = false;
+
+	// Use this for initialization
+	IEnumerator Start ()
+    {
+        do
+        {
+            yield return StartCoroutine(SpanwAllWaves());   //come back to start and continue with whatever is after this statement once this has completed.
+            //yield: it waits  until it's finished. Then it comes back to the while
+        }
+        while (looping);    //So once this completed, we come back here
+	}
+
+    private IEnumerator SpanwAllWaves()
+    {
+        for (int waveIndex = startingWave; waveIndex < waveConfigs.Count; waveIndex++)
+        { 
+            var currentWave = waveConfigs[waveIndex];
+            yield return StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+        }
+    }
+	
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        for (int enemyCount = 0; enemyCount < waveConfig.GetNumberOfEnemies(); enemyCount++)
+        {
+            var newEnemy = Instantiate(
+                waveConfig.GetEnemyPrefab(),
+                waveConfig.GetWaypoints()[0].transform.position,
+                Quaternion.identity);
+            newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig);
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
+    }
+
+}
